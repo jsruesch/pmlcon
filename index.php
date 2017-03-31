b8dbcd589319123885c506f344467508688e359e
jsruesch <jsruesch@gmail.com>
2017-03-31 10:19:20 -0500

    Add portraits and links

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <img class="center-block main-logo" src="img/logo.png" />
       </div>
 
-      <div class="row wow fadeInUp" data-wow-delay=".65s">
+      <div class="row wow fadeInUp mar60-bot" data-wow-delay=".65s">
         <div class="col-sm-6 col-sm-offset-3 geocontainer pad60 bg-white text-center">
           <h1 class="text-orange">Save the Date!</h1>
           <h2 class="text-mblue">August 14, 2017</h2>
@@ -17,133 +17,77 @@
         </div>
       </div>
 
-      <!-- <div class="row">
-        <div class="col-sm-6 geocontainer pad60 bg-white">
-          <h1 class="text-orange">Header</h1>
-          <h1 class="text-dblue">Header</h1>
-          <h1 class="text-mblue">Header</h1>
-          <h1 class="text-lblue">Header</h1>
-          <p class="text-dblue">
-            Body copy
-          </p>
-        </div>
-      </div>
-
-      <div class="row mar60-top">
-        <div class="col-sm-4 pad10">
-          <div class="row mar60-top pad30 bg-white">
-            <p>
-              Play
-            </p>
+      <!-- Portraits -->
+      <div class="row wow fadeInUp" data-wow-delay=".95s">
+        <!-- David Gagnon -->
+        <div class="col-5 pad10">
+          <div class="row">
+            <img class="full" src="img/portraits/portrait-david.png" />
+          </div>
+          <div class="full pad10 bg-white geocontainer">
+            <h3 class="text-center text-dblue">David<br />Gagnon</h3>
+            <h5 class="text-mblue text-center bold">
+              Field Day
+            </h5>
           </div>
         </div>
-        <div class="col-sm-4 pad10">
-          <div class="row mar60-top pad30 bg-white">
-            <p>
-              Make
-            </p>
-          </div>
-        </div>
-        <div class="col-sm-4 pad10">
-          <div class="row mar60-top pad30 bg-white">
-            <p>
-              Learn
-            </p>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="row">
-
-        <div class="col-xs-15 pad10">
-          <div class="row bg-white pad30">
+        <!-- Rich Halverson -->
+        <a href="http://personalizationinpractice.org/" target="_blank">
+          <div class="col-5 pad10">
             <div class="row">
-              <p>
-                picture
-              </p>
+              <img class="full" src="img/portraits/portrait-rich.png" />
             </div>
-            <div class="row">
-              <h3>Name</h3>
-            </div>
-            <div class="row">
-              <p>
-                logo
-              </p>
+            <div class="full pad10 bg-white geocontainer-02">
+              <h3 class="text-center text-dblue">Rich<br />Halverson</h3>
+              <h5 class="text-mblue text-center bold">
+                Personalization & Practice
+              </h5>
             </div>
           </div>
-        </div>
-        <div class="col-xs-15 pad10">
-          <div class="row bg-white pad30">
+        </a>
+        <!-- Erica Halverson -->
+        <a href="https://ci.education.wisc.edu/ci/people/faculty/erica-halverson" target="_blank">
+          <div class="col-5 pad10">
             <div class="row">
-              <p>
-                picture
-              </p>
+              <img class="full" src="img/portraits/portrait-erica.png" />
             </div>
-            <div class="row">
-              <h3>Name</h3>
-            </div>
-            <div class="row">
-              <p>
-                logo
-              </p>
+            <div class="full pad10 bg-white geocontainer">
+              <h3 class="text-center text-dblue">Erica<br />Halverson</h3>
+              <h5 class="text-mblue text-center bold">
+                Learning in the Making
+              </h5>
             </div>
           </div>
+        </a>
+        <!-- Dan Norton -->
+        <a href="https://www.filamentlearning.com" target="_blank">
+          <div class="col-5 pad10">
+            <div class="row">
+              <img class="full" src="img/portraits/portrait-dan.png" />
+            </div>
+            <div class="full pad10 bg-white geocontainer-02">
+              <h3 class="text-center text-dblue">Dan<br />Norton</h3>
+              <h5 class="text-mblue text-center bold">
+                Filament Games
+              </h5>
+            </div>
         </div>
-        <div class="col-xs-15 pad10">
-          <div class="row bg-white pad30">
+        </a>
+        <!-- Mike Beall -->
+        <a href="http://gearlearning.org/" target="_blank">
+          <div class="col-5 pad10">
             <div class="row">
-              <p>
-                picture
-              </p>
+              <img class="full" src="img/portraits/portrait-mike.png" />
             </div>
-            <div class="row">
-              <h3>Name</h3>
-            </div>
-            <div class="row">
-              <p>
-                logo
-              </p>
+            <div class="full pad10 bg-white geocontainer">
+              <h3 class="text-center text-dblue">Mike<br />Beall</h3>
+              <h5 class="text-mblue text-center bold">
+                Gear Learning
+              </h5>
             </div>
           </div>
-        </div>
-        <div class="col-xs-15 pad10">
-          <div class="row bg-white pad30">
-            <div class="row">
-              <p>
-                picture
-              </p>
-            </div>
-            <div class="row">
-              <h3>Name</h3>
-            </div>
-            <div class="row">
-              <p>
-                logo
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xs-15 pad10">
-          <div class="row bg-white pad30">
-            <div class="row">
-              <p>
-                picture
-              </p>
-            </div>
-            <div class="row">
-              <h3>Name</h3>
-            </div>
-            <div class="row">
-              <p>
-                logo
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </div> -->
-
+        </a>
+      </div><!-- END: Portraits -->
     </div><!-- /.container -->
   </div><!-- /.container-fluid .main-content -->
 <!-- END: Main Content -->
