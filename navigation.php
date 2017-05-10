2d447267b899524bdf8492a74a726dc6b774f893
jsruesch <jake@gofarwell.com>
2017-05-10 09:54:54 -0500

    make schedule point to overview

--- navigation.php
+++ navigation.php
@@ -20,7 +20,7 @@
         </div>
         <div class="col-xs-15">
           <div class="row">
-            <a href="schedule.php" class="text-white">
+            <a href="#schedule" class="text-white">
               <span class="text-orange"><i class="fa fa-calendar" aria-hidden="true"></i></span>
               <span class="nav-button-text">Schedule</span>
             </a>
