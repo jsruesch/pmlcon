0cc272c937eaae2ec2f19d329b236981ea7484bd
108sarahshaw <33810415+108sarahshaw@users.noreply.github.com>
2019-01-24 15:09:20 -0600

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- Nav Buttons -->
       <div class="row">
-        <div class="col-xs-15">
+        <div class="col-xs-3">
           <div class="row">
             <a href="/" class="text-white">
               <span class="text-orange"><i class="fa fa-home" aria-hidden="true"></i></span>
@@ -10,7 +10,7 @@
             </a>
           </div>
         </div>
-        <div class="col-xs-15">
+        <div class="col-xs-3">
           <div class="row bg-orange">
             <a href="https://registration.eop.education.wisc.edu/index.aspx?id=225" target="_blank" class="text-white">
               <span class="text-white"><i class="fa fa-check-circle-o" aria-hidden="true"></i></span>
@@ -26,7 +26,7 @@
             </a>
           </div>
         </div> -->
-        <div class="col-xs-15">
+        <div class="col-xs-3">
           <div class="row">
             <a href="/#lodging" class="text-white">
               <span class="text-orange"><i class="fa fa-bed" aria-hidden="true"></i></span>
@@ -34,7 +34,7 @@
             </a>
           </div>
         </div>
-        <div class="col-xs-15">
+        <div class="col-xs-3">
           <div class="row">
             <a href="/#keynote" class="text-white">
               <span class="text-orange"><i class="fa fa-user-circle-o" aria-hidden="true"></i></span>
